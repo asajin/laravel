01a2504a86c36e4800669404df460daf9ca3281d
blukacs <blukacs@pentalog.fr>
2015-10-05 15:32:33 +0300

    #Travis config

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'SomeRandomStringSomeRandomStringSomeRandomString'),
 
     'cipher' => 'AES-256-CBC',
 
